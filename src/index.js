f32fb08e057fca0c5195b2e72ddedcf1087f24ee
shiyananan <shiyananan@gmail.com>
2023-09-02 16:51:05 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let pair = bracketsConfig[i].join('');
+    brackets.push(pair);
+    };
+
+  let newStr = str;
+
+  while (newStr === str) {
+    for (let j = 0; j < brackets.length; j++) {
+      let thePair = brackets[j];
+      newStr = newStr.replace(thePair, '');
+    }
+    if (newStr != str) {
+      str = newStr;
+    } else {
+      return newStr.length === 0;
+    }
+  }
+
+};
